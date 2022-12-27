112e3a4548cba1ef115ad2f00519eb4f448fa062
Tushar12222296 <119563979+Tushar12222296@users.noreply.github.com>
2022-12-27 16:07:25 +0530

    Add files via upload
    A small python file to convert distance from Kilometers to Miles

---
+++ kmToMil.py
@@ -0,0 +1,4 @@
+# A python function to convert the distance from kilometers to miles
+a = float(input("Enter distance in kilometer: "))
+A = a*0.621371
+print('%0.3f kilometer is equal to %0.3f miles'%(a,A) )
